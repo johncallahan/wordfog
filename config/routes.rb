6ae7d2e4d37767d879e33320f5a878875605a7f1
John Callahan <jcallahan@acm.org>
2021-10-09 16:02:49 -0400

    adding logout, remove signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :fogs
+  devise_scope :user do
+    get 'logout', to: 'devise/sessions#destroy'
+  end
   root :to => "fogs#index"
 end
